c48fef1d64f5f485f29c5ebefd480850671412b6
atteez <atte-98@windowslive.com>
2024-11-13 17:28:20 +0200

    random api

--- screens/Random.js
+++ screens/Random.js
@@ -1,13 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '../style/style';
+import React, { useState, useEffect } from 'react'
+import { View, Text, Button } from 'react-native'
 
-export default function Random() {
-    return (
-      <View style={styles.container}>
-        <Text>Random</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+const App = () => {
+  const [ingredients, setIngredients] = useState([])
+  const [name, setName] = useState('')
+  const [refresh, setRefresh] = useState()
+
+  useEffect(() => {
+    fetch(URL) 
+      .then(response => response.json())
+      .then(data => {
+        const drink = data.drinks[0]
+        
+        setName(drink.strDrink)
+
+        const ingredientList = Object.keys(drink)
+          .filter(key => key.startsWith("strIngredient"))
+          .map(key => drink[key])
+          .filter(Boolean)
+
+        setIngredients(ingredientList)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [refresh])
+
+  const getNewCocktail = () => {
+    setRefresh({})
   }
+
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text>ingredients:</Text>
+      {ingredients.map((ingredient, index) => (
+        <Text key={index}>{ingredient}</Text>
+      ))}
+      <Button title='new Drink' onPress={getNewCocktail}/>
+    </View>
+  )
+}
+
+export default App
