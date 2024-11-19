ea39c0e024b746d5e9d465af799e41f5a6e6e9ea
atteez <atte-98@windowslive.com>
2024-11-19 15:26:35 +0200

    random screen update

--- screens/Random.js
+++ screens/Random.js
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, Button, Pressable } from 'react-native'
+import { View, Text, Button, Pressable, Image, ScrollView } from 'react-native'
 import styles from '../style/style'
 
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
-const App = () => {
+const Random = () => {
   const [ingredients, setIngredients] = useState([])
   const [name, setName] = useState('')
   const [refresh, setRefresh] = useState()
+  const [image, setImage] = useState()
+  const [measures, setMeasures] = useState([])
+  const [instructions, setInstructions] = useState('')
 
   useEffect(() => {
-    fetch(URL) 
+    fetch(URL)
       .then(response => response.json())
       .then(data => {
         const drink = data.drinks[0]
-        
+
         setName(drink.strDrink)
+        setImage(drink.strDrinkThumb)
+        setInstructions(drink.strInstructions)
 
         const ingredientList = Object.keys(drink)
           .filter(key => key.startsWith("strIngredient"))
           .map(key => drink[key])
           .filter(Boolean)
 
+        const measuretList = Object.keys(drink)
+          .filter(key => key.startsWith("strMeasure"))
+          .map(key => drink[key])
+          .filter(Boolean)
+
         setIngredients(ingredientList)
+        setMeasures(measuretList)
       })
       .catch(error => {
         console.log(error)
@@ -34,17 +45,25 @@ const App = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>{name}</Text>
-      <Text style={styles.text}>ingredients:</Text>
-      {ingredients.map((ingredient, index) => (
-        <Text style={styles.text} key={index}>{ingredient}</Text>
-      ))}
-      <Pressable style={styles.button} onPress={getNewCocktail}>
-        <Text>new Drink</Text>
-      </Pressable>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.header}>{name}</Text>
+        <Image source={{ uri: image }} style={styles.image} />
+        <Text style={styles.text}>ingredients:</Text>
+        {ingredients.map((ingredient, index) => (
+          <View key={index} style={styles.ingredientRow}>
+            <Text style={styles.ingredient}>{ingredient}</Text>
+            <Text style={styles.measure}>{measures[index] || ''}</Text>
+          </View>
+        ))}
+        <Text style={styles.text}> Instructions: </Text>
+        <Text>{instructions}</Text>
+        <Pressable style={styles.button} onPress={getNewCocktail}>
+          <Text>new Drink</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
   )
 }
 
-export default App
+export default Random
--- style/style.js
+++ style/style.js
@@ -3,37 +3,59 @@ import Constants from "expo-constants"
 
 export default StyleSheet.create({
     container: {
-    flex: 1,  
-    alignItems: 'center', 
-    backgroundColor: '#fff',
-},
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
     header: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: Constants.statusBarHeight + 10,
-    marginBottom: Constants.statusBarHeight + 5,
-},
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: Constants.statusBarHeight + 10,
+        marginBottom: Constants.statusBarHeight + 5,
+    },
     text: {
-    color: '#070000',
-    fontWeight: 'thin',
-    fontSize: 20,
-    textAlign: 'center',
-},
+        color: '#070000',
+        fontWeight: 'thin',
+        fontSize: 20,
+        textAlign: 'center',
+    },
     textinput: {
-    borderWidth: 2,
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    width: 250
-},
+        borderWidth: 2,
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+        width: 250
+    },
     button: {
-    margin: 10,
-    flexDirection: "row",
-    padding: 10,
-    backgroundColor: "#73CED6",
-    width: 100,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center'
-},
+        margin: 10,
+        flexDirection: "row",
+        padding: 10,
+        backgroundColor: "#73CED6",
+        width: 100,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 10
+    },
+    ingredientRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 20,
+        marginVertical: 5,
+    },
+    ingredient: {
+        fontSize: 16,
+        flex: 1,
+        textAlign: 'left',
+    },
+    measure: {
+        fontSize: 16,
+        flex: 1,
+        textAlign: 'right',
+    },
 });
