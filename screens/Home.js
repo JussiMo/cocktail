c649859cf32eda49046d55dbdef8a0a2d7fd80d9
jarkkomaatta <127930482+jarkkomaatta@users.noreply.github.com>
2024-11-20 11:27:49 +0200

    Home screen tyylia

--- screens/Home.js
+++ screens/Home.js
@@ -6,15 +6,14 @@ import styles from '../style/style';
 export default function Home() {
     return (
       <ScrollView contentContainerStyle={{  alignItems: 'center', backgroundColor: '#ffffff'}}>
-
         <Text style={styles.header}>Cocktail Quest</Text>
         <Text style={styles.text}>Shake things up and stir your imagination!</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>Dive into the vibrant world of cocktails, where every sip tells a story. Whether you’re in the mood for something classic, daring, or completely unexpected, we’ve got you covered.</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>✨ Feeling adventurous? Discover random cocktail recipes with a single tap.</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>🔍 On the hunt? Search for your perfect mix.</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>❤️ Found a favorite? Save it and build your personal collection of go-to drinks.</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>Cocktails aren’t just drinks—they’re experiences in a glass. Let’s craft your next unforgettable one!</Text>
-        <Text style={[styles.text, {marginTop: 30}]}>Cheers! 🥂</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'justify'}]}>Dive into the vibrant world of cocktails, where every sip tells a story. Whether you’re in the mood for something classic, daring, or completely unexpected, we’ve got you covered.</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'justify'}]}>✨ Feeling adventurous? Discover random cocktail recipes with a single tap.</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'justify'}]}>🔍 On the hunt? Search for your perfect mix.</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'justify'}]}>❤️ Found a favorite? Save it and build your personal collection of go-to drinks.</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'justify'}]}>Cocktails aren’t just drinks—they’re experiences in a glass. Let’s craft your next unforgettable one!</Text>
+        <Text style={[styles.text, {marginTop: 30, width: 320, textAlign: 'center', marginBottom: 100}]}>Cheers! 🥂</Text>
         <StatusBar style="auto" />
       </ScrollView>
     );
