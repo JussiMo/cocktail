d421cb4bffa619ce125b74fa1fa617aab62391ae
Jussi Molin <128314031+JussiMo@users.noreply.github.com>
2024-11-26 14:53:54 +0200

    test

--- screens/Profile.js
+++ screens/Profile.js
@@ -38,19 +38,19 @@ export default function Profile({ navigation }) {
       }
     });
   }, []);
-  
+
   const updateUserData = async () => {
     const colRef = collection(db, USERS_REF);
     await updateDoc(doc(colRef, auth.currentUser.uid), {
       nickname: nickname
     })
-    .then(() => {
-      Alert.alert("Account updated.")
-    })
-    .catch((error) => {
-      console.log("Update failed: " + error.message);
-      Alert.alert("Update failed: " + error.message);
-    })
+      .then(() => {
+        Alert.alert("Account updated.")
+      })
+      .catch((error) => {
+        console.log("Update failed: " + error.message);
+        Alert.alert("Update failed: " + error.message);
+      })
   }
 
   const handlePressLogout = () => {
@@ -88,21 +88,17 @@ export default function Profile({ navigation }) {
   if (!isLoggedIn) {
     return (
       <ScrollView>
-        <View>
-        <View style={styles.headerItem}>
-          <Text style={styles.header}>Profile</Text>
-        </View>
-        <Pressable style={styles.button}>
-          <Button
-            title="Login"
-            onPress={() => navigation.navigate('Login')} />
-        </Pressable>
-        <Text style={styles.infoText}>Dont have a profile yet?</Text>
-        <Pressable style={styles.button}>
-          <Button
-            title="Register"
-            onPress={() => navigation.navigate('Register')} />
-        </Pressable>
+        <View style={styles.container}>
+          <View style={styles.headerItem}>
+            <Text style={styles.header}>Profile</Text>
+          </View>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('Login')}>
+            <Text title="Login">LOGIN</Text>
+          </Pressable>
+          <Text style={styles.infoText}>Dont have a profile yet?</Text>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('Register')}>
+            <Text title="Register">REGISTER</Text>
+          </Pressable>
         </View>
       </ScrollView>
     )
@@ -110,65 +106,65 @@ export default function Profile({ navigation }) {
   else {
     return (
       <ScrollView>
-        <View>
-        <View style={styles.headerItem}>
-          <Text style={styles.header}>Profile</Text>
-          <Pressable style={styles.logout} onPress={handlePressLogout}>
-            <Text style={styles.logout}>Logout </Text>
-            <MaterialIcons name="logout" size={24} color="red" />
-          </Pressable>
-        </View>
-        <Text style={styles.header}>Update profile</Text>
-        <Text style={styles.label}>Nickname: {nickname}</Text>
-        <Text style={styles.label}>Email: {email}</Text>
-        <TextInput
-          value={nickname}
-          style={styles.textinput}
-          onChangeText={setNickname}
-        />
-        <View style={styles.button}>
-          <Button 
-            title="Update"
-            onPress={() => updateUserData()}
+        <View style={styles.container}>
+          <View style={styles.headerItem}>
+            <Text style={styles.header}>Profile</Text>
+            <Pressable style={styles.logout} onPress={handlePressLogout}>
+              <Text style={styles.logout}>Logout </Text>
+              <MaterialIcons name="logout" size={24} color="red" />
+            </Pressable>
+          </View>
+          <Text style={styles.header}>Update profile</Text>
+          <Text style={styles.label}>Nickname: {nickname}</Text>
+          <Text style={styles.label}>Email: {email}</Text>
+          <TextInput
+            value={nickname}
+            style={styles.textinput}
+            onChangeText={setNickname}
           />
-        </View>
-        <Text style={styles.header}>Change your password</Text>
-        <TextInput
-          style={styles.textinput}
-          placeholder="Enter your new password*"
-          value={password}
-          onChangeText={(password) => setPassword(password)}
-          secureTextEntry={true}
-        />
-        <TextInput
-          style={styles.textinput}
-          placeholder="Confirm your new password*"
-          value={confirmPassword}
-          onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
-          secureTextEntry={true}
-        />
-        <View style={styles.button}>
-          <Button 
-            title="Change password"
-            onPress={handlePressChangePw} />
-        </View>
-        <Text style={styles.header}>Delete account</Text>
-        <TextInput
-          style={styles.textinput}
-          placeholder="Type DELETE here to confirm"
-          value={confirmDelete}
-          onChangeText={(confirmDelete) => setConfirmDelete(confirmDelete)}
-          autoCapitalize="characters"
-        />
-        <View style={styles.button}>
-          <Button
-            title="Delete account"
-            color="red"
-            onPress={() => handlePressDelete()} />
-        </View>
-        <Text style={styles.infoText}>
-          Your data will be removed from the database!
-        </Text>
+          <View style={styles.button}>
+            <Button
+              title="Update"
+              onPress={() => updateUserData()}
+            />
+          </View>
+          <Text style={styles.header}>Change your password</Text>
+          <TextInput
+            style={styles.textinput}
+            placeholder="Enter your new password*"
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+            secureTextEntry={true}
+          />
+          <TextInput
+            style={styles.textinput}
+            placeholder="Confirm your new password*"
+            value={confirmPassword}
+            onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+            secureTextEntry={true}
+          />
+          <View style={styles.button}>
+            <Button
+              title="Change password"
+              onPress={handlePressChangePw} />
+          </View>
+          <Text style={styles.header}>Delete account</Text>
+          <TextInput
+            style={styles.textinput}
+            placeholder="Type DELETE here to confirm"
+            value={confirmDelete}
+            onChangeText={(confirmDelete) => setConfirmDelete(confirmDelete)}
+            autoCapitalize="characters"
+          />
+          <View style={styles.button}>
+            <Button
+              title="Delete account"
+              color="red"
+              onPress={() => handlePressDelete()} />
+          </View>
+          <Text style={styles.infoText}>
+            Your data will be removed from the database!
+          </Text>
         </View>
       </ScrollView>
     );
